b4d2fc1c40044f7c545d7ab73a01bfb312c63c1c
Michael Schoderer <schoderer@googlemail.com>
2015-10-23 09:58:45 +0200

    I18n base config

---
+++ src/main/java/de/schoderer/bookstore/db/BookPersistence.java
@@ -0,0 +1,9 @@
+package de.schoderer.bookstore.db;
+
+/**
+ * Created by michael on 23.10.15.
+ */
+public interface BookPersistence {
+
+
+}
---
+++ src/main/java/de/schoderer/bookstore/domain/Book.java
@@ -0,0 +1,7 @@
+package de.schoderer.bookstore.domain;
+
+/**
+ * Created by michael on 23.10.15.
+ */
+public class Book {
+}
