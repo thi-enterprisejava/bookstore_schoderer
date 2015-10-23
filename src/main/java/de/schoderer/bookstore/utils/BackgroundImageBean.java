ec21033cd3555c11cd1d68545db292c871d19970
Michael Schoderer <schoderer@googlemail.com>
2015-10-23 23:27:01 +0200

    BaseLayout

---
+++ src/main/java/de/schoderer/bookstore/utils/BackgroundImageBean.java
@@ -0,0 +1,21 @@
+package de.schoderer.bookstore.utils;
+
+import javax.ejb.Singleton;
+import javax.inject.Named;
+import java.util.Random;
+
+/**
+ * Created by schod on 23.10.2015.
+ */
+@Named
+@Singleton
+public class BackgroundImageBean {
+    private static final String BACKGROUND_IMAGE_TEMPLATE = "background-image: url(img/book_%NUMBER%.jpg);";
+    private static final Random random = new Random();
+
+
+    public String getRandomImage(){
+        return BACKGROUND_IMAGE_TEMPLATE.replace("%NUMBER%", String.valueOf(random.nextInt(4)));
+    }
+
+}
