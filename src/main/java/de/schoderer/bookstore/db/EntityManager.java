639ff2d1ea997e733716d9e36bdb259a05cac99e
Michael Schoderer <schoderer@googlemail.com>
2015-11-29 14:31:08 +0100

    Started DB Integration

--- src/main/java/de/schoderer/bookstore/db/EntityManager.java
+++ src/main/java/de/schoderer/bookstore/db/EntityManager.java
@@ -1,32 +1,45 @@
 package de.schoderer.bookstore.db;
 
 import de.schoderer.bookstore.domain.Book;
+import de.schoderer.bookstore.utils.interceptor.TimeLogging;
 
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Named;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.util.List;
 
 /**
  * Created by schod on 21.11.2015.
  */
+@TimeLogging
+@Named
+@Stateless
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
 public class EntityManager implements BookPersistence {
 
     @PersistenceContext
     private javax.persistence.EntityManager em;
 
-
     @Override
     public List<Book> fetchAllBooks() {
-        return null;
+        TypedQuery<Book> query = em.createQuery("SELECT b FROM Book as b", Book.class);
+        return query.getResultList();
     }
-
     @Override
     public List<Book> fetchAllBooksByTitle(String title) {
-        return null;
+        TypedQuery<Book> query = em.createQuery("SELECT b FROM Book as b where b.title like :booktitle", Book.class);
+        query.setParameter("booktitle", title);
+        return query.getResultList();
     }
-
     @Override
     public List<Book> fetchAllBooksByTag(String tag) {
+        // TODO
+        // /TypedQuery<Book> query = em.createQuery("SELECT b FROM Book as b where b.tags = :booktitle", Book.class);
+        //query.setParameter("booktitle", title);
         return null;
     }
 
@@ -42,6 +55,7 @@ public class EntityManager implements BookPersistence {
     }
 
     @Override
+    @Transactional
     public void updateBook(Book book) {
         em.merge(book);
     }
---
+++ src/main/java/de/schoderer/bookstore/utils/interceptor/TimeLoggingInterceptor.java
@@ -0,0 +1,25 @@
+package de.schoderer.bookstore.utils.interceptor;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+/**
+ * Created by michael on 29.11.15.
+ */
+
+
+@TimeLogging
+@Interceptor
+public class TimeLoggingInterceptor {
+    @AroundInvoke
+    public Object profile(InvocationContext ic) throws Exception {
+        long start = System.currentTimeMillis();
+        try {
+            return ic.proceed();
+        } finally {
+            long end = System.currentTimeMillis();
+            System.out.println(String.format("Called method %s of class %s took:\t ms", ic.getMethod().getName(),ic.getMethod().getDeclaringClass().getCanonicalName(), end-start));
+        }
+    }
+}
