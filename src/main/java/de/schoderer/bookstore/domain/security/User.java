5e902d0acebe36f4918cfbf8c534b226a07d0eb8
Michael Schoderer <schoderer@googlemail.com>
2015-12-17 09:49:54 +0100

    Started Security

---
+++ src/main/java/de/schoderer/bookstore/domain/security/User.java
@@ -0,0 +1,91 @@
+package de.schoderer.bookstore.domain.security;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * Created by michael on 17.12.2015.
+ */
+@Entity
+public class User implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private Date createdDate;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) &&
+                Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", createdDate=" + createdDate +
+                '}';
+    }
+}
---
+++ src/main/java/de/schoderer/bookstore/model/PageSwitcherBean.java
@@ -0,0 +1,86 @@
+package de.schoderer.bookstore.model;
+
+import de.schoderer.bookstore.utils.Pages;
+import org.apache.log4j.Logger;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+/**
+ * Created by michael on 10.11.15.
+ */
+@Named
+@SessionScoped
+public class PageSwitcherBean implements Serializable {
+    private static final Logger LOG = Logger.getLogger(PageSwitcherBean.class);
+
+    private static final String ACTIVE_CSS_CLASS = "active";
+    private String addBookPage = "";
+    private String indexPage = "active";
+    private String listResultsPage = "";
+
+    private Pages lastPage;
+    private Pages currentPage;
+
+
+    public String switchPage(String page) {
+        return switchPage(Pages.valueOf(page));
+    }
+
+    public String switchPage(Pages page) {
+        lastPage=currentPage;
+        currentPage=page;
+        resetCssClassStrings();
+        changeCSS(page);
+        if (LOG.isInfoEnabled()) {
+            LOG.info("Switched page to: " + page.toString());
+        }
+        return page.getFileName()+"?faces-redirect=true";
+    }
+
+    private void changeCSS(Pages page) {
+        switch (page) {
+            case INDEX:
+                indexPage = ACTIVE_CSS_CLASS;
+                break;
+            case LIST:
+                listResultsPage = ACTIVE_CSS_CLASS;
+                break;
+            case ADD:
+                addBookPage = ACTIVE_CSS_CLASS;
+                break;
+        }
+    }
+
+
+    private void resetCssClassStrings() {
+        addBookPage = "";
+        indexPage = "";
+        listResultsPage = "";
+    }
+
+    public String getAddBookPage() {
+        return addBookPage;
+    }
+
+    public void setAddBookPage(String addBookPage) {
+        this.addBookPage = addBookPage;
+    }
+
+    public String getIndexPage() {
+        return indexPage;
+    }
+
+    public void setIndexPage(String indexPage) {
+        this.indexPage = indexPage;
+    }
+
+    public String getListResultsPage() {
+        return listResultsPage;
+    }
+
+    public void setListResultsPage(String listResultsPage) {
+        this.listResultsPage = listResultsPage;
+    }
+}
