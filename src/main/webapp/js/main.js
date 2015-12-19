ee5afdfe8e07626cba28ae8a23fdbc535438590f
Michael Schoderer <schoderer@googlemail.com>
2015-12-19 22:11:44 +0100

    Mobile Menu Edit-Book-Button only for authorized users

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,8 +1,3 @@
-$("input[type=file].imagesOnlyMax6MB").attr("accept", "image/*").on("change", function () {
-    var max = 6 * 1024 * 1024; //6MB image only!!!
-
-    if (this.files && this.files[0].size > max) {
-        alert("File too large."); // DIRTY!!!
-        this.value = null;
-    }
+$(document).ready(function(){
+    $(".button-collapse").sideNav();
 });
